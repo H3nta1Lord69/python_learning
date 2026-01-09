cc510a6825a598e8215cc5d25a5762ecfec4ffd8
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2026-01-08 20:51:22 -0500

    chore: comments in python

---
+++ Fundamentals/01-first_steps/02-comments.py
@@ -0,0 +1,7 @@
+
+# This is how to do single-line comments in Python
+"""
+This is how to do multi-line comments in Python
+You can use triple quotes for this purpose
+"""
+print("Comments are important for code documentation!")
