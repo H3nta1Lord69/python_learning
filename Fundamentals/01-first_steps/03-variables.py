7d00cbbaafd314f9b70825a74d147486978935e4
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2026-01-08 21:00:16 -0500

    chore: variables in python

---
+++ Fundamentals/01-first_steps/03-variables.py
@@ -0,0 +1,33 @@
+
+greet = "Hello, World!"
+print(greet) # This code defines a variable 'greet' and assigns it the string "Hello, World!".
+# It then prints the value of the variable to the console.
+# Variables are used to store data that can be referenced and manipulated in a program.
+# In this case, 'greet' is a variable that holds a greeting message.
+# You can change the value of the variable at any time:
+greet = "Hi there!"
+print(greet) # This will now print "Hi there!" instead of "Hello, World!".
+# Variables can store different types of data, such as numbers, strings, lists, etc.
+number = 42
+print(number) # This will print the number 42.
+# You can also perform operations using variables:
+sum_result = number + 8
+print(sum_result) # This will print 50, which is the result of adding 42 and 8.
+# Remember to choose meaningful variable names to make your code more readable!
+
+# PEP8 recommends using lowercase letters with underscores for variable names.
+user_name = "Carlos"
+print(user_name) # This will print "Carlos".
+# Avoid using reserved keywords as variable names, such as 'print', 'if', 'for', etc.
+# This can lead to unexpected behavior in your code.
+# Instead, use descriptive names that convey the purpose of the variable.
+age = 25
+print(age) # This will print 25.
+# In summary, variables are essential for storing and manipulating data in programming.
+# They help make your code dynamic and flexible.
+
+_private_name = "This is a private variable"
+print(_private_name) # This will print "This is a private variable".
+# The underscore at the beginning indicates that this variable is intended for internal use.
+# However, it can still be accessed from outside its intended scope.
+# Use variables wisely to enhance the functionality of your programs!
