b393bca0d6afab9576dac9f790abebdc80725d5a
H3nta1Lord69 <carlos.saenz.g99@gmail.com>
2026-01-13 21:54:53 -0500

    chore: math operations in python

---
+++ Fundamentals/01-first_steps/10-math_operations.py
@@ -0,0 +1,44 @@
+
+# sum 
+a = 5
+b = 3
+print(a + b)
+
+# difference
+a = 10
+b = 4
+print(a - b)
+
+# product
+a = 7
+b = 6
+print(a * b)
+
+# quotient
+a = 20
+b = 5
+print(a / b)
+
+# floor division - rounds down to the nearest whole number
+a = 22
+b = 7
+print(a // b)
+
+# modulus - remainder of the division
+a = 17
+b = 5
+print(a % b)
+
+# exponentiation - power
+a = 3
+b = 4
+print(a ** b)
+
+# rounding
+num = 5.6789
+print(round(num, 2))  # rounds to 2 decimal places
+print(round(num))     # rounds to nearest integer
+
+# absolute value
+num = -15
+print(abs(num))
